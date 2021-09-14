02040a1e27a7cee9671a440dcc51a958f383dd58
henro47 <53427025+henro47@users.noreply.github.com>
2021-09-14 15:58:43 +0200

    keras model working

--- ITRI_671/views.py
+++ ITRI_671/views.py
@@ -6,12 +6,14 @@ import pandas as pd
 import numpy as np
 import sys
 
+# KERAS API FUNCTIONS
+
 EPOCHS = 117
 BATCH_SIZE = 224
 
 def load_model(model_path):
     try:
-        model = keras.load_model(model_path)
+        model = keras.models.load_model('ITRI_671\keras_model')
         return model
     except:
         print('failed to load model')
@@ -29,9 +31,38 @@ def predict(model, data):
     predictions = model.predict(x=data, batch_size=BATCH_SIZE, verbose=0)
     return predictions
 
+
+#VIEWS
+
 def home(request):
-    model = load_model('keras_model')
-    return render(request, "home.html")
+    return render(request, 'home.html')
 
 def prediction(request):
+    model = load_model('keras_model')
+    model.summary()
+    input_data = []
+
+    input_data.append(float(request.GET.get('t_h',1)))
+    input_data.append(float(request.GET.get('t_p',1)))
+    input_data.append(float(request.GET.get('pr',1)))
+    input_data.append(float(request.GET.get('hu',1)))
+    input_data.append(float(request.GET.get('yw',1)))
+    input_data.append(float(request.GET.get('pi',1)))
+    input_data.append(float(request.GET.get('ro',1)))
+    input_data.append(float(request.GET.get('m_x',1)))
+    input_data.append(float(request.GET.get('m_y',1)))
+    input_data.append(float(request.GET.get('m_z',1)))
+    input_data.append(float(request.GET.get('acc_x',1)))
+    input_data.append(float(request.GET.get('acc_y',1)))
+    input_data.append(float(request.GET.get('acc_z',1)))
+    input_data.append(float(request.GET.get('gy_x',1)))
+    input_data.append(float(request.GET.get('gy_y',1)))
+    input_data.append(float(request.GET.get('gy_z',1)))
+
+    df = pd.DataFrame([input_data])
+    print(df.head())
+    pred = predict(model, df)
+    print(pred)
     return render(request, "prediction.html")
+
+	
